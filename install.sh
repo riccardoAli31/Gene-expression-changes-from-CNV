93a4610641daf094be022dc4e92984ada10e267f
Marcus Wagner <marcus.wagner@tum.de>
2024-12-17 14:11:42 +0100

    adapted installation files, because 'igraph' is not compatible with conda; implemented barcode filtering; debugging copykat call

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-# script to install software for cmscb project 8
-conda create -n cmscb scanpy python-igraph leidenalg r-base r-essentials
-
-# custom R packages with BioConductor
-conda activate cmscb
+# Script to install R software for CNV extraction with
+#  CopyKat and epiAneufinder for Unix based OS.
+# Note: CopyKat depends on the igraph package, which
+#  does not work well with conda environments. Thus,
+#  please install this to your machine or a container.
+sudo apt install r-base r-essentials r-cran-irkernel
 
 # install R packages from separate script file
 Rscript install.R
-
