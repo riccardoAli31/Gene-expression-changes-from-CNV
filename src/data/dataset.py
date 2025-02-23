f0c33bb40d4edcbdcc6cc7be8c33cf6e9a6d7d39
Marcus Wagner <marcus.wagner@tum.de>
2025-02-23 09:06:19 +0100

    implemented save_embedding() and load_embedding() methods; polished code

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,10 +1,11 @@
-import torch
-from .embedding import embed
-from pathlib import Path
-import pandas as pd
+#!/ust/bin/python
 from typing import Union, List
+from pathlib import Path
+from pandas import DataFrame
+from numpy import ndarray
+import torch
 from scipy.io import mmread, mmwrite
-import gzip
+from .embedding import embed
 
 
 # TODO:
@@ -14,14 +15,14 @@ import gzip
 # file size of one single_gene_barcode matrix: 561463 bytes
 
 
-class CopyNumerDNADataset(torch.utils.data.Dataset):
+class CnvDataset(torch.utils.data.Dataset):
     """
     Dataset class for DNA, ATAC and CNV data.
     """
 
-    def __init__(self, root, data_df: pd.DataFrame, *args,
+    def __init__(self, root, data_df: DataFrame, *args,
                  force_recompute=False, embedding_mode='single_gene_barcode',
-                 verbose=1, **kwargs):
+                 file_format='mtx', verbose=1, **kwargs):
         """
         Initialization funciton.
         Computes embeddings from raw data, if needed. In this case use kwargs:
@@ -72,8 +73,8 @@ class CopyNumerDNADataset(torch.utils.data.Dataset):
             file_paths = list()
             for barcode, gene_id, embedding in embedder:
                 if verbose > 2:
-                    print('{}: {} embedding for {}, {}'.format(
-                        i, embedding.shape, barcode, gene_id
+                    print('{} embedding for {}, {}'.format(
+                        embedding.shape, barcode, gene_id
                     ))
                 match embedding_mode:
                     case 'single_gene_barcode':
@@ -81,63 +82,85 @@ class CopyNumerDNADataset(torch.utils.data.Dataset):
                         if not file_dir.exists():
                             file_dir.mkdir(parents=True)
                         
-                        torch.save(
-                            torch.from_numpy(embedding), 
-                            file_dir / (gene_id + '.pt')
+                        f_path = self.save_embedding(
+                            embedding, file_dir, gene_id, file_format
                         )
-                        file_paths.append(
-                            file_dir / (gene_id + '.pt')
-                        )
+                        file_paths.append(f_path)
                     case 'gene_concat':
                         file_dir = self.root_path
                         if not file_dir.exists():
                             file_dir.mkdir(parents=True)
                         
-                        torch.save(
-                            torch.from_numpy(embedding), 
-                            file_dir / (barcode + '.pt')
+                        f_path = self.save_embedding(
+                            embedding, file_dir, barcode, file_format
                         )
-                        file_paths.append(
-                            file_dir / (barcode + '.pt')
-                        )
+                        file_paths.append(f_path)
                     case 'barcode_channel':
                         file_dir = self.root_path / barcode
                         if not file_dir.exists():
                             file_dir.mkdir(parents=True)
 
-                        torch.save(
-                            torch.from_numpy(embedding), 
-                            file_dir / (gene_id + '.pt')
+                        f_path = self.save_embedding(
+                            embedding, file_dir, gene_id, file_format
                         )
-                        file_paths.append(
-                            file_dir / (gene_id + '.pt')
-                        )
+                        file_paths.append(f_path)
 
             print(len(file_paths))
             print(self.data_df.shape)
             self.data_df['embedding_path'] = file_paths
 
         else:
-            pass
-            # TODO: add file path column to self.data_df based on mode and
-            #   the barcode + gene_id columns
-            # create table with files
-            # match embedding_mode:
-            #     case 'single_gene_barcode':
-            #         self.data_df['embedding_path'] = [
-            #                     self.root_path / barcode / gene + '.pt'
-            #                     for gene in gene_ids
-            #                 ]
-            #     case 'gene_concat':
-            #         self.data_df['embedding_path'] = [
-            #                     self.root_path / cell + '.pt'
-            #                     for cell in barcode_ids
-            #                 ]
-            #     case 'barcode_channel':
-            #         self.data_df['embedding_path'] = [
-            #                     self.root_path / gene + '.pt'
-            #                     for gene in gene_ids
-            #                 ]
+            # add file path column to self.data_df based on mode
+            match embedding_mode:
+                case 'single_gene_barcode':
+                    self.data_df['embedding_path'] = [
+                        self.root_path / barcode / (gene + '.' + file_format)
+                        for gene in gene_ids
+                    ]
+                case 'gene_concat':
+                    self.data_df['embedding_path'] = [
+                        self.root_path / (cell + '.' + file_format)
+                        for cell in barcode_ids
+                    ]
+                case 'barcode_channel':
+                    self.data_df['embedding_path'] = [
+                        self.root_path / (gene + '.' + file_format)
+                        for gene in gene_ids
+                    ]
+    
+    @staticmethod
+    def save_embedding(embedding: ndarray, out_dir: Path, file_name: str,
+                        file_format:str='mtx') -> Path:
+        """
+        Function to save embedding to file.
+
+        TODO: add gzip functionality
+        """
+
+        file_path = out_dir / (file_name + '.' + file_format)
+        match file_format:
+            case 'pt':
+                torch.save(torch.from_numpy(embedding), file_path)
+            case 'mtx':
+                mmwrite(file_path, embedding)
+        return file_path
+    
+    @staticmethod
+    def load_embedding(file_path: Path) -> torch.Tensor:
+        """
+        Load an embedding from file depending on the file format.
+
+        TODO:
+        * gunzip for decompression
+        """
+
+        file_format = file_path.name.split('.')[-1]
+        match file_format:
+            case 'pt':
+                return torch.load(file_path)
+            case 'mtx':
+                return torch.from_numpy(mmread(file_path))
+        raise RuntimeError('Unsupported file format: {}'.format(file_format))
 
     @staticmethod
     def _subset_embedding_rows(n_rows: int, dna=True, atac=True, cnv=True):
@@ -175,14 +198,15 @@ class CopyNumerDNADataset(torch.utils.data.Dataset):
             the _subset_embedding_rows() function.
         """
 
-        embedding = torch.load(data_df.loc[idx]['embedding_path'])
+        file_path = data_df.loc[idx]['embedding_path']
+        embedding = CnvDataset.load_embedding(file_path)
 
         if rows is not None:
             return embedding[rows,:]
         return embedding
 
     @staticmethod
-    def _get_grund_truth_label(data_df: pd.DataFrame, idx: int,
+    def _get_grund_truth_label(data_df: DataFrame, idx: int,
                                type='classification'):
         """
         Return the ground truth respective to regression or classification.
@@ -210,10 +234,13 @@ class CopyNumerDNADataset(torch.utils.data.Dataset):
             'label': self._get_grund_truth_label(self.data_df, idx)
         }
 
-    def split(self, train, test, val=None):
-        """
-        Function to create training, validation and test splits.
-        """
-        # TODO: write function to make test/val/train splits
-        pass
+    def __str__(self):
+        return '{} with {} datapoints'.format(self.__class__, len(self))
+
+    # def split(self, train, test, val=None):
+    #     """
+    #     Function to create training, validation and test splits.
+    #     """
+    #     # TODO: write function to make test/val/train splits
+    #     pass
                 
